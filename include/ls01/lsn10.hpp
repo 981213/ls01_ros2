adb7fcbdaf715090a081a6b5dfef74535c3bf422
Chuanhong Guo <gch981213@gmail.com>
2023-04-21 14:17:56 +0800

    n10: determine params from packet
    in preparation for n10 plus support

--- include/ls01/lsn10.hpp
+++ include/ls01/lsn10.hpp
@@ -9,27 +9,30 @@
 #include <ls01/ls01.hpp>
 
 #define LSN10_PKT_LEN 58
-#define LSN10_RESOL 80
-#define LSN10_RESOL_RAD (0.8 / 360 * 2 * M_PI);
-#define LSN10_MEASUREMENT_CNT (36000 / LSN10_RESOL)
 #define LSN10_MEASUREMENT_PER_PKT 16
+#define LSN10_ANGLE_STABLE_COUNT 500
 
 namespace LS01 {
-    class LSN10 : public LS01 {
+    class LSN10Common : public LS01 {
     public:
-        explicit LSN10(const rclcpp::NodeOptions &options);
+        LSN10Common(const std::string &node_name, const rclcpp::NodeOptions &options, speed_t baud_option,
+                    int _measurement_per_pkt)
+                : LS01(node_name, options, baud_option), measurement_per_pkt(_measurement_per_pkt) {}
 
-        ~LSN10() override;
+        ~LSN10Common() override;
 
-    private:
-
+    protected:
         void recv_thread();
 
+        std::vector<uint8_t> pkt_buffer;
+    private:
         int start() const;
 
         int stop() const;
 
         int process_packet();
+
+        int probe_params();
 
         void prepare_packet();
 
@@ -37,7 +40,24 @@ namespace LS01 {
 
         rclcpp::Time msg_time;
         rclcpp::Time last_pkt_time;
-        std::vector<uint8_t> pkt_buffer;
-        int last_stop_angle = 0;
+        uint32_t last_stop_angle = 0;
+        int measurement_per_pkt;
+
+        // lidar param probing
+        uint32_t angle_increment = 0;
+        uint32_t angle_stable_count = 0;
+        uint32_t measurement_cnt;
+        float angle_incr_rad = 0;
+        bool lidar_params_determined = false;
+
+    };
+
+    class LSN10 : public LSN10Common {
+    public:
+        explicit LSN10(const rclcpp::NodeOptions &options)
+                : LSN10Common("lsn10", options, B230400, LSN10_MEASUREMENT_PER_PKT) {
+            pkt_buffer.resize(LSN10_PKT_LEN);
+            lidar_thread = std::thread(&LSN10::recv_thread, this);
+        }
     };
 }
--- src/lsn10.cpp
+++ src/lsn10.cpp
@@ -11,19 +11,13 @@
 #include <unistd.h>
 
 namespace LS01 {
-    LSN10::LSN10(const rclcpp::NodeOptions &options) : LS01("lsn10", options, B230400) {
-        pkt_buffer.resize(LSN10_PKT_LEN);
-        set_angle_multiplier(1.0 / LSN10_RESOL * 100);
-        lidar_thread = std::thread(&LSN10::recv_thread, this);
-    }
-
-    LSN10::~LSN10() {
+    LSN10Common::~LSN10Common() {
         terminate_thread = true;
         lidar_thread.join();
         stop();
     }
 
-    void LSN10::recv_thread() {
+    void LSN10Common::recv_thread() {
         size_t buf_ptr = 0;
         ssize_t rdlen;
         enum class _sync_t {
@@ -58,7 +52,6 @@ namespace LS01 {
                     if (pkt_buffer[0] == 0xa5) {
                         sync_state = _sync_t::SYNC2;
                         last_pkt_time = get_clock()->now();
-                        prepare_packet();
                     }
                     break;
                 case _sync_t::SYNC2:
@@ -85,7 +78,10 @@ namespace LS01 {
                         break;
                     } else if ((size_t) rdlen == pkt_left) {
                         buf_ptr = 0;
-                        ret = process_packet();
+                        if (lidar_params_determined)
+                            ret = process_packet();
+                        else
+                            ret = probe_params();
                         if (ret) {
                             RCLCPP_WARN(get_logger(), "invalid packet.");
                             sync_state = _sync_t::SYNC1;
@@ -98,7 +94,7 @@ namespace LS01 {
         }
     }
 
-    int LSN10::start() const {
+    int LSN10Common::start() const {
         static const uint8_t start_motor[] = {0xa5, 0x5a, 0x55, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                               0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                               0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -118,7 +114,7 @@ namespace LS01 {
         return serial_write(start_motor, sizeof(start_motor));
     }
 
-    int LSN10::stop() const {
+    int LSN10Common::stop() const {
         static const uint8_t stop_motor[] = {0xa5, 0x5a, 0x55, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                                              0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -138,47 +134,78 @@ namespace LS01 {
         return serial_write(stop_motor, sizeof(stop_motor));
     }
 
-    void LSN10::prepare_packet() {
+    void LSN10Common::prepare_packet() {
         msg_time = last_pkt_time;
         scan_msg = std::make_unique<sensor_msgs::msg::LaserScan>();
         scan_msg->header.stamp = msg_time;
         scan_msg->header.frame_id = lidar_frame;
         scan_msg->angle_min = 0;
-        scan_msg->angle_increment = LSN10_RESOL_RAD;
-        scan_msg->angle_max = 2 * M_PI - LSN10_RESOL_RAD;
+        scan_msg->angle_increment = angle_incr_rad;
+        scan_msg->angle_max = 2 * M_PI - angle_incr_rad;
         scan_msg->range_min = 0.1;
         scan_msg->range_max = 12.0;
-        scan_msg->intensities.resize(LSN10_MEASUREMENT_CNT, 0);
-        scan_msg->ranges.resize(LSN10_MEASUREMENT_CNT, 0);
+        scan_msg->intensities.resize(measurement_cnt, 0);
+        scan_msg->ranges.resize(measurement_cnt, 0);
     }
 
-    void LSN10::finish_packet() {
+    void LSN10Common::finish_packet() {
         scan_msg->scan_time = (last_pkt_time - msg_time).seconds();
-        scan_msg->time_increment = scan_msg->scan_time / (float) LSN10_MEASUREMENT_CNT;
+        scan_msg->time_increment = scan_msg->scan_time / (float) measurement_cnt;
         filter_scan();
         scan_pub->publish(std::move(scan_msg));
     }
 
-    int LSN10::process_packet() {
+    int LSN10Common::probe_params() {
+        if (pkt_buffer[0] != 0xa5 || pkt_buffer[1] != 0x5a)
+            return -EINVAL;
+
+        uint32_t start_angle = pkt_buffer[5] << 8 | pkt_buffer[6];
+        uint32_t stop_angle = pkt_buffer[pkt_buffer.size() - 3] << 8 | pkt_buffer[pkt_buffer.size() - 2];
+        uint32_t angle_diff = (stop_angle < start_angle ? stop_angle + 36000 : stop_angle) - start_angle;
+        // division that rounds to the closest integer
+        uint32_t cur_angle_increment = (angle_diff + (measurement_per_pkt - 1) / 2) / (measurement_per_pkt - 1);
+
+        if (cur_angle_increment != angle_increment) {
+            angle_increment = cur_angle_increment;
+            angle_stable_count = 0;
+            RCLCPP_DEBUG(get_logger(), "Motor speed unstable. Current resolution: %.2f degree.", angle_increment / 100.0);
+        } else {
+            angle_stable_count++;
+        }
+
+        if(angle_stable_count > LSN10_ANGLE_STABLE_COUNT) {
+            lidar_params_determined = true;
+            float angle_incr_degree = angle_increment / 100.0f;
+            angle_incr_rad = angle_incr_degree / 360 * 2 * M_PI;
+            measurement_cnt = 36000 / angle_increment;
+            RCLCPP_INFO(get_logger(), "Motor stabilized. Resolution: %.2f degree.", angle_incr_degree);
+            set_angle_multiplier(1.0f / angle_incr_degree);
+            prepare_packet();
+        }
+        return 0;
+    }
+
+    int LSN10Common::process_packet() {
         if (pkt_buffer[0] != 0xa5 || pkt_buffer[1] != 0x5a)
             return -EINVAL;
         last_pkt_time = get_clock()->now();
-        int start_angle = pkt_buffer[5] << 8 | pkt_buffer[6];
-        int scan_ptr = (start_angle + LSN10_RESOL / 2) / LSN10_RESOL;
+        uint32_t start_angle = pkt_buffer[5] << 8 | pkt_buffer[6];
+        uint32_t scan_ptr = (start_angle + angle_increment / 2) / angle_increment;
         if (start_angle < last_stop_angle) {
             finish_packet();
             prepare_packet();
         }
-        last_stop_angle = pkt_buffer[55] << 8 | pkt_buffer[56];
+        last_stop_angle = pkt_buffer[pkt_buffer.size() - 3] << 8 | pkt_buffer[pkt_buffer.size() - 2];
+
         auto data_ptr = &pkt_buffer[7];
-        for (int i = 0; i < LSN10_MEASUREMENT_PER_PKT; i++) {
-            if (scan_ptr >= LSN10_MEASUREMENT_CNT) {
+        for (int i = 0; i < measurement_per_pkt; i++) {
+            if (scan_ptr >= measurement_cnt) {
                 scan_ptr = 0;
                 finish_packet();
                 prepare_packet();
             }
             // the scan data angle is clockwise while LaserScan message expects ccw. Invert it.
-            int idx = LSN10_MEASUREMENT_CNT - scan_ptr - 1;
+            uint32_t idx = measurement_cnt - scan_ptr - 1;
             scan_msg->ranges[idx] = ((data_ptr[0] << 8) | data_ptr[1]) / 1000.0f;
             scan_msg->intensities[idx] = data_ptr[2];
             scan_ptr++;
